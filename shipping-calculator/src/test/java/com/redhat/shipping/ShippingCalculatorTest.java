0a68434a9c592b3ba341394fae3aec262962f13e
Ferhat Sanuc <ferhat.sanuc@turktelekom.com.tr>
2022-01-26 07:44:01 -0500

    Refactor

--- shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
+++ shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
@@ -5,6 +5,51 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ShippingCalculatorTest {
+	@Test
 
-    // @todo: add tests
+	public void canCalculateTheCostForARegion() {
+
+		ShippingCalculator calculator = new ShippingCalculator();
+
+		assertEquals(0, calculator.costForRegion("A Region"));
+
+	}
+
+	@Test
+
+	public void onNARegionTheCostIs100() {
+		// Given a shipping calculator
+		 ShippingCalculator calculator = new ShippingCalculator();
+		 // When NA is the country region
+		 int calculatedCost = calculator.costForRegion("NA");
+		 // Then the shipping cost is 100
+		 assertEquals(100, calculatedCost);
+
+
+	}
+
+	@Test
+
+	public void onLATAMRegionTheCostIs200() {
+
+		// Given a shipping calculator
+		 ShippingCalculator calculator = new ShippingCalculator();
+		 // When LATAM is the country region
+		 int calculatedCost = calculator.costForRegion("LATAM");
+		 // Then the shipping cost is 200
+		 assertEquals(200, calculatedCost);
+	 }
+
+	@Test
+
+	public void onEMEARegionTheCostIs300() {
+
+		// Given a shipping calculator
+		 ShippingCalculator calculator = new ShippingCalculator();
+		 // When EMEA is the country region
+		 int calculatedCost = calculator.costForRegion("EMEA");
+		 // Then the shipping cost is 300
+		 assertEquals(300, calculatedCost);
+		 }
+
 }
